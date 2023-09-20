e81d9b4ea9aa0442e74da8736e3524f51fbb682e
HonzikNatyi <mtpnati@gmail.com>
2023-09-20 18:07:08 +0200

    first commit

---
+++ vue-project/src/App.vue
@@ -0,0 +1,40 @@
+<template>
+    <div id="app">
+      <input type="text" v-model="searchText"
+      placeholder="Search..">
+      <list :items="items" :searchText="searchText"></list>
+    </div>
+</template>
+
+<script>
+import list from './components/list.vue';
+export default {
+  name: "App",
+  components: { list },
+  data() {
+    return {
+      searchText: '',
+      items: [
+      {
+    "id": "1",
+    "name": "Smartphone X1",
+    "description": "High-quality smartphone with dual-camera",
+    "availability": "In Stock",
+    "price": 599.99
+  },
+  {
+    "id": "2",
+    "name": "Electric Toothbrush",
+    "description": "Advanced bristle technology for a better clean",
+    "availability": "Out of Stock",
+    "price": 49.99
+  },
+      ]
+    }
+  },
+};
+</script>
+
+
+
+
---
+++ vue-project/src/components/list.vue
@@ -0,0 +1,47 @@
+<template>
+    <article v-for="item in filterItems(items)">
+        <h3>{{ item.id }}</h3>
+        <h3>{{ item.name }}</h3>
+        <h3>{{ item.description }}</h3>
+        <h3 class="stock">{{ item.availability }}</h3>
+        <h3 class="price">{{ item.price }}</h3>
+    </article>
+</template>
+
+<script>
+export default {
+    props: ["items", "searchText"],
+    methods:{
+        filterItems(items){
+            const searchText = this.searchText;
+            if (searchText ===""){
+                return items;
+            }
+           return items.filter((item) => {
+                if (item.name.indexOf(searchText) !== -1)
+
+                return true;
+
+                if (item.description.indexOf(searchText) !== -1)
+
+                return true;   
+
+                if (item.availability.indexOf(searchText) !== -1)
+
+                return true;
+
+                if (new String(item.price).indexOf(searchText) !== -1)
+
+                return true;
+                
+                if (new String(item.id).indexOf(searchText) !== -1)
+                
+                return true;
+
+                return false;
+
+            })
+        }
+    }
+};
+</script>
